de90d51240a3f6b8335b8c4fc45b0c4b5b556796
Anand <as504919@gmail.com>
2024-01-29 10:22:58 +0530

    Added functionalities.

--- script.js
+++ script.js
@@ -1,189 +1,160 @@
-const splideList = document.querySelector(".splide__list");
-
-(async function generateSlider() {
-  async function fetchData() {
-    const result = await fetch(
-      "https://api.themoviedb.org/3/movie/popular?api_key=3a8866f399840a6b6a592f097f506f6c"
-    );
-    const res = await result.json();
-    console.log(res.results);
-    return res.results;
-
-    // console.log(res);
-  }
-
-  let data = await fetchData();
-  console.log(data);
-
-  //   Getting all the genres from teh TMDB api and storing them as an array:
-  const allGenres = [
-    {
-      id: 28,
-      name: "Action",
-    },
-    {
-      id: 12,
-      name: "Adventure",
-    },
-    {
-      id: 16,
-      name: "Animation",
-    },
-    {
-      id: 35,
-      name: "Comedy",
-    },
-    {
-      id: 80,
-      name: "Crime",
-    },
-    {
-      id: 99,
-      name: "Documentary",
-    },
-    {
-      id: 18,
-      name: "Drama",
-    },
-    {
-      id: 10751,
-      name: "Family",
-    },
-    {
-      id: 14,
-      name: "Fantasy",
-    },
-    {
-      id: 36,
-      name: "History",
-    },
-    {
-      id: 27,
-      name: "Horror",
-    },
-    {
-      id: 10402,
-      name: "Music",
-    },
-    {
-      id: 9648,
-      name: "Mystery",
-    },
-    {
-      id: 10749,
-      name: "Romance",
-    },
-    {
-      id: 878,
-      name: "Science Fiction",
-    },
-    {
-      id: 10770,
-      name: "TV Movie",
-    },
-    {
-      id: 53,
-      name: "Thriller",
-    },
-    {
-      id: 10752,
-      name: "War",
-    },
-    {
-      id: 37,
-      name: "Western",
-    },
-  ];
-
-  function getCurrentGenre(id) {
-    // console.log(id);
-    const currentGenres = [];
-    for (let i = 0; i < id.length; i++) {
-      for (let j = 0; j < allGenres.length; j++) {
-        if (allGenres[j].id === id[i]) {
-          currentGenres.push(allGenres[i].name);
-        }
-      }
-    }
-    // console.log(currentGenres);
-    return currentGenres;
-  }
-
-  async function getCurrentImage(poster_path) {
-    const image = await fetch(`https://image.tmdb.org/t/p/w300/${poster_path}`);
-
-    // console.log(image.url);
-    return image.url;
-  }
-
-  splideList.innerHTML = data
-    .map((item) => {
-      //   console.log(item);
-      const currentItemImage = getCurrentImage(item.poster_path);
-      //   console.log(currentItemImage);
-      const currentGenre = getCurrentGenre(item.genre_ids).join(",");
-      //   console.log(currentGenre);
-      return `<div class="splide__slide">
-        <img src='https://image.tmdb.org/t/p/w300//9GBhzXMFjgcZ3FdR9w3bUMMTps5.jpg]' class="item-image" alt="Item Image"/>
-        <p class="item-title">${item.title}</p>
-        <p>${currentGenre}</p>
-        <div class="splide-slide-overlay" onclick="addFavourite(${item.id})">
-        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none" class="favourite-btn">
-  <path fill-rule="evenodd" clip-rule="evenodd" d="M3.17157 5.48284C4.73367 3.96185 7.26633 3.96185 8.82842 5.48284L9.99999 6.62359L11.1716 5.48284C12.7337 3.96185 15.2663 3.96185 16.8284 5.48284C18.3905 7.00383 18.3905 9.46984 16.8284 10.9908L9.99999 17.6396L3.17157 10.9908C1.60948 9.46984 1.60948 7.00383 3.17157 5.48284Z" fill="#D1D5DB"/>
-</svg></div>
-    </div>`;
-    })
-    .join("");
-
-  // Function to search the movie:
-
-  //   function displaySearchedMovie() {
-  const searchText = document.getElementById("search-bar").value;
-  console.log(searchText);
-  const searchButton = document.querySelector(".search-btn");
-  searchButton.addEventListener("click", searchMovie);
-
-  async function searchMovie() {
-    const movie = await fetch(
-      `https://api.themoviedb.org/3/search/movie?api_key=3a8866f399840a6b6a592f097f506f6c&query=The%20Shawshank%20Redemption`
-    );
-    console.log(movie);
-  }
-  //   }
-
-  //   displaySearchedMovie();
-
-  //   Function to set the favourites part:
-  let basket = JSON.parse(localStorage.getItem("data"))
-    ? JSON.parse(localStorage.getItem("data"))
-    : [];
-
-  function addFavourite(id) {
-    basket.push({ id: id, item: 1 });
-  }
-
-  const favouritesSection = document.querySelector(".favourites-section");
-
-  basket.map((item) => {
-    return;
+function genreStr(myGenre) {
+  let genreStr = [];
+  myGenre.map((genreID) => {
+    genreStr.push(myMap.get(genreID));
   });
-})();
+  return genreStr.join(", ");
+}
 
-const splide = new Splide(".splide", {
-  type: "loop",
-  perPage: 4,
-  gap: "80px",
-  width: "100%",
-  arrows: true,
-  prev: true,
-  permove: 4,
-  breakpoints: {
-    700: {
-      width: "100%",
-    },
-    480: {
-      width: "100%",
-    },
+let myMap = new Map();
+
+const urlGenres = "https://api.themoviedb.org/3/genre/movie/list?language=en";
+const optionsGenres = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MGRhMDI0MDI3OWQwMTQ5Y2JlZjIzNDlhMzBiNWVlYSIsInN1YiI6IjU2Y2ZjYWJiYzNhMzY4MWU0NDAwNTQ4MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.N4fQwyEQMyNvkGSD2SDJBf9K_iHlt0WcZw0yFZktmkw",
   },
+};
+
+fetch(urlGenres, optionsGenres)
+  .then((res) => res.json())
+  .then((json) => {
+    json.genres.map((data) => {
+      myMap.set(data.id, data.name);
+    });
+  })
+  .catch((err) => console.error("error:" + err));
+
+// Recommended section:
+let imgPath = `https://image.tmdb.org/t/p/original`;
+const url2 =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc";
+const options2 = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MGRhMDI0MDI3OWQwMTQ5Y2JlZjIzNDlhMzBiNWVlYSIsInN1YiI6IjU2Y2ZjYWJiYzNhMzY4MWU0NDAwNTQ4MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.N4fQwyEQMyNvkGSD2SDJBf9K_iHlt0WcZw0yFZktmkw",
+  },
+};
+
+fetch(url2, options2)
+  .then((res) => res.json())
+  .then((json) => {
+    let str = "";
+
+    json.results.map((movie) => {
+      str += ` <li class="splide__slide my-li-slide">
+                        <div class="inside-li">
+                            <div class="li-image">
+                            <img class="li-img" src=${
+                              imgPath + movie.poster_path
+                            } alt="">
+                            </div>
+                            <h3>${movie.original_title}</h3>
+                            <p>${genreStr(movie.genre_ids)}</p>
+                        </div>
+                    </li>`;
+    });
+    document.querySelector('ul[data-ref="ul"]').innerHTML = str;
+    var splide = new Splide(".splide", {
+      width: "100%",
+      perPage: 4,
+      gap: "5em",
+      autoWidth: true,
+      pagination: false,
+    });
+    splide.mount();
+  });
+
+document.querySelector(".search-div").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const value = document.querySelector(".search-input").value;
+  const moviesData = [];
+  let movieGenres = [];
+  let imgPath = `https://image.tmdb.org/t/p/original`;
+  const url = `https://api.themoviedb.org/3/search/movie?query=${value}`;
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MGRhMDI0MDI3OWQwMTQ5Y2JlZjIzNDlhMzBiNWVlYSIsInN1YiI6IjU2Y2ZjYWJiYzNhMzY4MWU0NDAwNTQ4MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.N4fQwyEQMyNvkGSD2SDJBf9K_iHlt0WcZw0yFZktmkw",
+    },
+  };
+
+  fetch(url, options)
+    .then((res) => res.json())
+    .then((json) => {
+      moviesData.push(json.results[0]);
+      document.querySelector(".search-poster").src =
+        imgPath + json.results[0].poster_path;
+      document.querySelector(".title").innerText =
+        json.results[0].original_title;
+      document.querySelector(".discription").innerText =
+        json.results[0].overview;
+      document.querySelector(".relese-date").innerText =
+        json.results[0].release_date;
+      movieGenres = json.results[0].genre_ids;
+      document.querySelector(".category").innerText = genreStr(
+        json.results[0].genre_ids
+      );
+    })
+    .then((data) => {
+      const url2 =
+        "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc" +
+        "&with_genres=" +
+        encodeURI(movieGenres.join(","));
+      const options2 = {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MGRhMDI0MDI3OWQwMTQ5Y2JlZjIzNDlhMzBiNWVlYSIsInN1YiI6IjU2Y2ZjYWJiYzNhMzY4MWU0NDAwNTQ4MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.N4fQwyEQMyNvkGSD2SDJBf9K_iHlt0WcZw0yFZktmkw",
+        },
+      };
+
+      fetch(url2, options2)
+        .then((res) => res.json())
+        .then((json) => {
+          let str = "";
+
+          json.results.map((movie) => {
+            str += ` <li class="splide__slide my-li-slide">
+                        <div class="inside-li">
+                            <div class="li-image">
+                            <img class="li-img" src=${
+                              imgPath + movie.poster_path
+                            } alt="">
+                            </div>
+                            <h3>${movie.original_title}</h3>
+                            <p>${genreStr(movie.genre_ids)}</p>
+                        </div>
+                    </li>`;
+          });
+          document.querySelector('ul[data-ref="ul"]').innerHTML = str;
+          var splide = new Splide(".splide", {
+            type: "slide",
+            width: "100%",
+            perPage: 4,
+            gap: "1.5rem",
+            autoWidth: true,
+            pagination: false,
+          });
+          splide.mount();
+          document.querySelector(".search-input").value = "";
+        })
+        .catch((err) => console.error("error:" + err));
+    })
+    .catch((err) => console.error("error:" + err));
+});
+
+var splide = new Splide(".splider-two", {
+  width: "100%",
+  gap: "5em",
+  autoWidth: true,
+  pagination: false,
 });
 splide.mount();
